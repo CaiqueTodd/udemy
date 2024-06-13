1c5b61fc129c6bc9f4a7251953f1b29f19c695a4
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-06-12 21:51:57 -0300

    Comentários de código

---
+++ javascript/javascript-basico/002_comentarios-de-codigo/comentarios.js
@@ -0,0 +1,13 @@
+// Comentário para única linha
+const comentario = "Hello, world!" // comenta somente oque está após as duas barras
+
+
+/* Comentário para mais de uma linha*/
+
+/*
+if (comentario == "Hello, world!") {
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
