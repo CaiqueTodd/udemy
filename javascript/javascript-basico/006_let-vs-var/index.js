df441e044ef5c0f5b4dbf4406e7eb14231c1d0a8
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-06-13 21:55:20 -0300

    Diferenças entre var e let

---
+++ javascript/javascript-basico/006_let-vs-var/index.js
@@ -0,0 +1,8 @@
+// Var pode ser declarado mais de uma vez
+// Let pode ser declarado somente uma vez
+var nome = "Caique";
+var nome = "Livia";
+console.log(nome);
+
+// Variável global
+idade = 24; //NÃO FAÇA ISSO, SEMPRE DIGA SE É LET OU CONST
