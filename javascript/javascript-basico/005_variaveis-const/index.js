11b307f2f90c4c07b4ca721e820eeca64504cc37
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-06-13 21:36:21 -0300

    Constantes com const

---
+++ javascript/javascript-basico/005_variaveis-const/index.js
@@ -0,0 +1,13 @@
+// 1. Não podemos criar constantes com palavras reservada (Ex.: let, if, console, etc...)
+// 2. Constantes precisam ter nomes significativos
+// 3. O nome de uma constante não pode iniciar com número (Ex.: 1nome)
+// 4. O nome de uma constante deve seguir o camel case (Ex.: nomeCliente)
+// 5. Constante possui letras case-sensitive (O motor do JavaScript interpreta letra minuscula diferente de letra maiuscula)
+// 6. Não podemos redeclarar variáveis com const
+// 7. Não podemos modificar o valor de uma constante
+// 7. NÃO UTILIZAR VAR, UTILIZAR CONST
+
+const nome = "João";
+console.log(`${nome}`);
+// nome = "Caique"; 
+// Const não pode ter seu valor modificado
