5e611f4d0219fc6affa534ec10d8a1182d39853a
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-07-01 23:15:34 -0300

    Tipo de dados primitivos

---
+++ javascript/javascript-basico/007_dados-primitivos/index.js
@@ -0,0 +1,11 @@
+// String, number, undefined, null, boolean
+
+const nome = 'Caique'; // String
+const nome1 = "Caique"; // String
+const nome2 = `Caique`; // String
+const num1 = 10; // number
+const num2 = 10.52 // number
+let nomeAluno; // undefined = não aponta pra lugar nenhum na memória
+let sobrenomeAluno = null; // Nulo = não aponta pra lugar nenhum na memória
+const aprovado = true; // Boolean = true, false (lógico)
+
