fcaec602bab477deac22f3ef8a37c5d16fa2f5b6
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-07-02 22:51:55 -0300

    Resolvendo exercício de soma

---
+++ javascript/javascript-basico/009_alert-confirm-prompt/js/script.js
@@ -0,0 +1,5 @@
+alert(`Mensagem de alerta`);
+
+confirm(`Mensagem de confirmação`);
+
+prompt(`Inserir dado`)
---
+++ javascript/javascript-basico/exercicios/003_soma-de-valores/js/script.js
@@ -0,0 +1,5 @@
+let num1 = parseFloat(prompt(`Digite um número:`));
+let num2 = parseFloat(prompt(`Digite outro número:`))
+let resultado = num1 + num2
+
+alert(`A soma dos dois valores é: ${resultado}`)
