51d4918b2b4ffc009983d87eb0fecf7910a2c117
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-07-10 21:48:51 -0300

    Se aprofundando em Numbers

---
+++ javascript/javascript-basico/011_numbers/script.js
@@ -0,0 +1,12 @@
+let num1 = 10.5; // number
+let num2 = 2.5; // number
+
+// console.log(num1.toString() + num2); // converte número para string temporariamente
+// num1 = num1.toString(); // convertendo variável number para string
+// console.log(num1.toString(2)); // Ver representação binária
+// console.log(num1.toFixed(3)); // Escolher quantidade de casa decimal
+// console.log(Number.isInteger(num1)); // Retorna se o número é inteiro ou não (True / False)
+
+let temp = num1 * 'Ola';
+console.log(Number.isNaN(temp)); // Retorna se a expressão é NaN ou não (True / False)
+
