eef3348a3a718a19210f552804c3e18cc3ab8c29
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-07-17 23:52:36 -0300

    Introdução básica de Funções

---
+++ javascript/javascript-basico/014_funcoes/script_1.js
@@ -0,0 +1,7 @@
+function saudacao(nome) {
+    return `Bom dia, ${nome}`;
+}
+
+const variavel = saudacao('Caique')
+
+console.log(variavel)
---
+++ javascript/javascript-basico/014_funcoes/script_3.js
@@ -0,0 +1,17 @@
+// 1. Maneira
+const raizQd = function (n) {
+    return Math.sqrt(n);
+};
+console.log(raizQd(25));
+
+
+// 2. Maneira
+const raizQuadrada = (n) => {
+    return Math.sqrt(n);
+};
+console.log(raizQuadrada(9))
+
+
+// 3. Maneira
+const raiz = n => Math.sqrt(n);
+console.log(raiz(100));
